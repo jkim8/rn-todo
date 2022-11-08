61803801695f04587e842cf75505b9a88c8dff62
jkim8 <juno0218@naver.com>
2022-11-08 16:01:36 -0500

    HeaderLeftButton component

---
+++ src/components/HeaderLeftButton.js
@@ -0,0 +1,27 @@
+import { useNavigation } from '@react-navigation/native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Pressable } from 'react-native';
+import PropTypes from 'prop-types';
+
+const HeaderLeftButton = ({ canGoBack, tintColor }) => {
+  const navigation = useNavigation();
+  if (!canGoBack) {
+    return null;
+  }
+  return (
+    <Pressable onPress={navigation.goBack} hitSlop={10}>
+      <MaterialCommunityIcons
+        name="chevron-left"
+        size={30}
+        color={tintColor}
+      />
+    </Pressable>
+  );
+};
+
+HeaderLeftButton.propTypes = {
+  canGoBack: PropTypes.bool,
+  tintColor: PropTypes.string,
+};
+
+export default HeaderLeftButton;
