274d18fb0e8d29af4674c28a0cef23da9a1d8ee4
jkim8 <juno0218@naver.com>
2022-11-08 13:24:31 -0500

    Alert

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,5 +1,11 @@
 import { useEffect, useRef, useState } from 'react';
-import { Image, Keyboard, StyleSheet, View } from 'react-native';
+import {
+  Alert,
+  Image,
+  Keyboard,
+  StyleSheet,
+  View,
+} from 'react-native';
 import { signIn } from '../api/auth';
 import Button from '../components/Button';
 import Input, {
@@ -27,10 +33,17 @@ const SignInScreen = () => {
       try {
         const data = await signIn(email, password);
         console.log(data);
+        setIsLoading(false);
       } catch (e) {
-        console.log(e);
+        Alert.alert('SignIn Error', e, [
+          {
+            text: 'OK',
+            onPress: () => {
+              setIsLoading(false);
+            },
+          },
+        ]);
       }
-      setIsLoading(false);
     }
   };
 
