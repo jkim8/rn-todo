4500282632e0c9ba6a4f60fa309c32f67119dac4
jkim8 <juno0218@naver.com>
2022-11-09 09:18:51 -0500

    signout buton

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,16 +5,30 @@ import {
   Text,
 } from 'react-native';
 import PropTypes from 'prop-types';
-import { GRAY, PRIMARY, WHITE } from '../colors';
+import { DANGER, GRAY, PRIMARY, WHITE } from '../colors';
 
-const Button = ({ title, onPress, disabled, isLoading }) => {
+export const ButtonTypes = {
+  PRIMARY: 'PRIMARY',
+  DANGER: 'DANGER',
+};
+
+const Button = ({
+  title,
+  onPress,
+  disabled,
+  isLoading,
+  buttonType,
+}) => {
+  const colors = { PRIMARY, DANGER };
+
   return (
     <Pressable
       onPress={onPress}
       style={({ pressed }) => [
         styles.container,
-        pressed && { backgroundColor: PRIMARY.DARK },
-        disabled && { backgroundColor: PRIMARY.LIGHT },
+        { backgroundColor: colors[buttonType].DEFAULT },
+        pressed && { backgroundColor: colors[buttonType].DARK },
+        disabled && { backgroundColor: colors[buttonType].LIGHT },
       ]}
       disabled={disabled}
     >
@@ -27,11 +41,16 @@ const Button = ({ title, onPress, disabled, isLoading }) => {
   );
 };
 
+Button.defaultProps = {
+  buttonType: ButtonTypes.PRIMARY,
+};
+
 Button.propTypes = {
   title: PropTypes.string.isRequired,
   onPress: PropTypes.func.isRequired,
   disabled: PropTypes.bool,
   isLoading: PropTypes.bool,
+  buttonType: PropTypes.oneOf(Object.values(ButtonTypes)),
 };
 
 const styles = StyleSheet.create({
